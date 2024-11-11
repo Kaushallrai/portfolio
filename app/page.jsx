5f7f5698e0706ebbb372d4d51079a0c25638f454
Kaushallrai <Kaushallrai03@gmail.com>
2024-11-12 00:50:29 +0545

    intro

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,55 @@
-export default function Home() {
-  return <main>homepage</main>;
-}
+import { Button } from "@/components/ui/button";
+import Social from "@/components/Social";
+import { FiDownload } from "react-icons/fi";
+import Photo from "@/components/Photo";
+
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1">
+              Hello I'm
+              <br />
+              <span className="text-accent">Kaushal Rai</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I'm a passionate software developer with a strong background in
+              computer science and a love for technology. I can help you build
+              your next project with expertise and creativity.
+            </p>
+            {/* button and socials */}
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download Resume</span>
+                <FiDownload
+                  className="text-xl
+                "
+                />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Social
+                  containerStyles="flex gap-6"
+                  iconStyles="w-14 h-14 border border-accent rounded-full flex justify-center items-center text-accent text-3xl hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                />
+              </div>
+            </div>
+          </div>
+          {/* photo */}
+          <div>
+            <Photo />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
