454fe09343da076734d1aaa016a6bdf6a935965c
Kaushallrai <Kaushallrai03@gmail.com>
2024-11-13 16:09:20 +0545

    a

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,7 +1,25 @@
 "use client";
 
+import { motion } from "framer-motion";
+import Image from "next/image";
+
 const Photo = () => {
-  return <div>Photo</div>;
+  return (
+    <div className="w-full h-full relative">
+      <motion.div>
+        <div className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten">
+          <Image
+            src="/assets/killua.png"
+            priority
+            quality={100}
+            fill
+            alt=""
+            className="object-contain"
+          />
+        </div>
+      </motion.div>
+    </div>
+  );
 };
 
 export default Photo;
